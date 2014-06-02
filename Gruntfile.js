eaf698a71c8c93cc4b260b793ce8ba6e71a7ffbb
Troels Lenda <troelslenda@gmail.com>
2014-06-02 14:21:13 +0200

    Building up basic files for theme

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Watch for changes and trigger compass with livereload on CSS files.
+    watch: {
+      scss: {
+        options: {
+          livereload: false
+        },
+        files: ['stylesheets/scss/*.scss'],
+        tasks: ['compass']
+      },
+      css: {
+        files: ['stylesheets/*.css'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+
+    // Compass and SCSS
+    compass: {
+      options: {
+        httpPath: '/profiles/ding2/themes/bang',
+        cssDir: 'stylesheets',
+        sassDir: 'stylesheets/scss',
+        assetCacheBuster: 'none'
+      },
+      prod: {
+        options: {
+          environment: 'production',
+          outputStyle: 'compact',
+          force: true,
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('default', [
+    'compass',
+    'watch'
+  ]);
+};
