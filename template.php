adef91d1193d89b0790c2f17cf2fe4f3435e4fcc
Troels Lenda <troelslenda@gmail.com>
2014-06-18 09:56:40 +0200

    Changing to typekit

--- template.php
+++ template.php
@@ -6,12 +6,12 @@
 function bang_preprocess_html(&$vars) {
 
   // TypeKit.
-  /*drupal_add_js('//use.typekit.net/qqr6kuv.js');
+  drupal_add_js('//use.typekit.net/qqr6kuv.js');
   $data = 'try{Typekit.load();}catch (e){}';
-  drupal_add_js($data, array('type' => 'inline'));*/
+  drupal_add_js($data, array('type' => 'inline'));
 
 
-  drupal_add_js('http://fast.fonts.net/jsapi/9b1cfb60-2d2f-4073-9fd4-43df10260641.js');
+  //drupal_add_js('http://fast.fonts.net/jsapi/9b1cfb60-2d2f-4073-9fd4-43df10260641.js');
 }
 
 
