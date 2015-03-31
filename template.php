b1fca8cca055b986c341711d3f69149acc8cb2f2
Kasper Garnaes <kasper.garnaes@gmail.com>
2015-03-31 05:41:15 +0200

    Only apply special styling for form elements if they have a label
    This prevents them from disappearing.
    
    - Add form-item-has-label class to form elements if they have a label
    - Only apply special styling if they have this class
    
    KKB-157.

--- template.php
+++ template.php
@@ -110,8 +110,105 @@ function bang_preprocess_field(&$vars) {
 }
 
 /**
+ * Implements hook_preprocess_form_element().
+ */
+function bang_preprocess_form_element(&$vars) {
+  $element = &$vars['element'];
+
+
+  if (empty($element['#attributes']['class'])) {
+    $element['#attributes']['class'] = array();
+  }
+
+  // Add class to form item if it has a label.
+  if (!empty($element['#title'])) {
+    $element['#attributes']['class'][] = 'form-item-has-label';
+  }
+}
+
+/**
  * Implements hook_js_alter().
  */
 function bang_js_alter(&$js) {
   unset($js['profiles/ding2/themes/ddbasic/scripts/ddbasic.topbar.menu.min.js']);
 }
+
+/**
+ * Implements theme_form_element().
+ *
+ * This is basically copy/pasted from theme_form_element with the addition of
+ * basing wrapper attributes on the #attributes value. This allows preprocess
+ * functions to add classes to form elements.
+ */
+function bang_form_element($variables) {
+  $element = &$variables['element'];
+
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
+
+  // This is where changes have been introduced.
+  // Base attributes on the #attributes value for the element and remember to
+  // initialize the classes array.
+  $attributes = $element['#attributes'];
+  if (!isset($attributes['class'])) {
+    $attributes['class'] = array();
+  }
+
+  // Add element #id for #type 'item'.
+  if (isset($element['#markup']) && !empty($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+  // Add element's #type and #name as class to aid with JS/CSS selectors.
+  // Add the base form-item class to the array.
+  $attributes['class'][] = 'form-item';
+  if (!empty($element['#type'])) {
+    $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
+  }
+  if (!empty($element['#name'])) {
+    $attributes['class'][] = 'form-item-' . strtr($element['#name'], array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
+  }
+  // Add a class for disabled elements to facilitate cross-browser styling.
+  if (!empty($element['#attributes']['disabled'])) {
+    $attributes['class'][] = 'form-disabled';
+  }
+
+  $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+  $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
+  $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
+
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  if (!empty($element['#description'])) {
+    $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+  }
+
+  $output .= "</div>\n";
+
+  return $output;
+}
+
